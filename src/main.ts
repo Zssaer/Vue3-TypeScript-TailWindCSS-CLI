20485505d2926fca3e19a464c76858113594f7dd
Zssaer <ntc001@163.com>
2022-05-13 17:00:49 +0800

    add ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import "@/assets/css/main.css";
 import { createPinia } from "pinia";
 import { router, setupRouter } from "./router";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App);
 app.use(createPinia());
+app.use(ElementPlus);
 
 setupRouter(app);
 router.isReady().then(() => app.mount("#app", true));
